878e575ec539ecefaf1b931687b72f02afc34e04
pierre delarocque <delarocque.pierre@gmail.com>
2024-07-03 18:10:23 +0200

    ajout image

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,11 +1,18 @@
 import IshiharaTest from "../components/IshiraTest";
 
+import datas from "../js/data";
+
 function Experience() {
   return (
     <>
-      <h1>Expériences</h1>
+      <h1 className="text-5xl font-bold ">Expériences</h1>
       <section>
-        <h2>Vu des daltoniens</h2>
+        <h2 className="text-4xl font-bold ">Vu des daltoniens</h2>
+        {datas.map((datas) => (
+          <section className="flex-row" key={datas.id}>
+            <img className="w-20" src={datas.images} alt={datas.titre} />
+          </section>
+        ))}
       </section>
       <section>
         <h2>Test d'Ishihara</h2>
