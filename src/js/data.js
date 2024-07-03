c3e033dacc558510f11086cda4b85e510ab177f0
pierre delarocque <delarocque.pierre@gmail.com>
2024-07-03 16:16:21 +0200

    ajout test ishira

---
+++ src/js/data.js
@@ -0,0 +1,20 @@
+const daltoniensList = [
+  {
+    titre: "Normal",
+    images: "",
+  },
+  {
+    titre: "Deuteranomaly",
+    images: "",
+  },
+  {
+    titre: "Protanopia",
+    images: "",
+  },
+  {
+    titre: "Tritanopia",
+    images: "",
+  },
+];
+
+export default daltoniensList;
