293c8352d4dff042b2b8685627d3f00bed2a69a2
pierre delarocque <delarocque.pierre@gmail.com>
2024-07-03 14:37:06 +0200

    stup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./App";
+import Accueil from "./pages/Accueil";
+import About from "./pages/About";
+import Realisation from "./pages/Realisation";
+import Contact from "./pages/Contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Accueil />,
+      },
+      {
+        path: "/daltonisme",
+        element: <Daltonisme />,
+      },
+      {
+        path: "/experience",
+        element: <Experience />,
+      },
+      {
+        path: "/equipe",
+        element: <Equipe />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
