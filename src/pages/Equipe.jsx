1a7ec3a05c1e44fd7b513fdaef4466c07b90da64
Nicolas-jchr <Nicolas.jchr@gmail.com>
2024-07-03 17:30:29 +0200

    Team page created

--- src/pages/Equipe.jsx
+++ src/pages/Equipe.jsx
@@ -1,7 +1,28 @@
 function Equipe() {
   return (
     <>
-      <h1>Hello je suis la page équipe</h1>
+    <header>
+      <h1>Qui sommes nous ?</h1>
+      <p>Nous sommes quatre développeurs web en formation à la Wild Code School. Nous avons conçu ce projet lors d'un Hackathon dont le thème était l'accessibilité. Notre équipe, appelée "Les Chill Bangers Peppers", a utilisé React et Tailwind pour développer ce site web. Nous avons concentré nos efforts pour créer une application qui non seulement répond aux critères modernes de développement web, mais qui est également accessible à tous les utilisateurs, indépendamment de leurs capacités. Ce projet représente notre engagement envers des solutions inclusives et notre passion pour l'innovation technologique.</p>
+      </header>
+      <main>
+        <section>
+          <h2>Mégane AUTHEMAYOU</h2>
+          <img src="" alt="membre de l'équipe" />
+        </section>
+        <section>
+          <h2>Pierre DELAROCQUE</h2>
+          <img src="" alt="membre de l'équipe" />
+        </section>
+        <section>
+          <h2>Amel BONNEVIE</h2>
+          <img src="" alt="membre de l'équipe" />
+        </section>
+        <section>
+          <h2>Nicolas JUCHEREAU</h2>
+          <img src="" alt="membre de l'équipe" />
+        </section>
+      </main>
     </>
   );
 }
