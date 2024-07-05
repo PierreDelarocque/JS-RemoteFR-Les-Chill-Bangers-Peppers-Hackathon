86057e30af491595ef7e6181d3f6b0cd1ca5731b
Mégane <megane.authemayou@gmail.com>
2024-07-05 09:57:48 +0200

    fixed good name with pictures

--- src/pages/Experience1.jsx
+++ src/pages/Experience1.jsx
@@ -3,9 +3,12 @@ import chien from "../assets/images/chien.png";
 function Experience1() {
   return (
     <>
-      <h1 className="text-4xl font-bold flex justify-center mb-4 mt-2">Expérience 1/4</h1>
+      <h1 className="text-4xl font-bold flex justify-center mb-4 mt-2">
+        Expérience 1/4
+      </h1>
       <h2 className="text-1xl font-bold flex justify-center mb-4">
-        Vous allez pouvoir observer comment chaque type de daltonisme peut percevoir une image
+        Vous allez pouvoir observer comment chaque type de daltonisme peut
+        percevoir une image
       </h2>
       <div className="flex justify-center items-start space-x-8">
         <div className="flex flex-col space-y-20 mt-10">
@@ -28,14 +31,14 @@ function Experience1() {
             <div className="absolute right-2 -top-4 group-hover:top-1 group-hover:right-2 z-10 w-32 h-32 rounded-full group-hover:scale-150 duration-500 bg-sky-800"></div>
             <div className="absolute -right-12 top-4 group-hover:top-1 group-hover:right-2 z-10 w-24 h-24 rounded-full group-hover:scale-150 duration-500 bg-sky-700"></div>
             <div className="absolute right-20 -top-4 group-hover:top-1 group-hover:right-2 z-10 w-16 h-16 rounded-full group-hover:scale-150 duration-500 bg-sky-600"></div>
-            <p className="z-10">Protanopia</p>
+            <p className="z-10">Tritanopia</p>
           </button>
           <button className="border hover:scale-95 duration-300 relative group cursor-pointer text-sky-50 overflow-hidden h-11 w-44 rounded-md bg-sky-200 p-2 flex justify-center items-center font-extrabold">
             <div className="absolute right-32 -top-4 group-hover:top-1 group-hover:right-2 z-10 w-40 h-40 rounded-full group-hover:scale-150 duration-500 bg-sky-900"></div>
             <div className="absolute right-2 -top-4 group-hover:top-1 group-hover:right-2 z-10 w-32 h-32 rounded-full group-hover:scale-150 duration-500 bg-sky-800"></div>
             <div className="absolute -right-12 top-4 group-hover:top-1 group-hover:right-2 z-10 w-24 h-24 rounded-full group-hover:scale-150 duration-500 bg-sky-700"></div>
             <div className="absolute right-20 -top-4 group-hover:top-1 group-hover:right-2 z-10 w-16 h-16 rounded-full group-hover:scale-150 duration-500 bg-sky-600"></div>
-            <p className="z-10">Tritanopia</p>
+            <p className="z-10">Protanopia</p>
           </button>
         </div>
         <img className="w-96 h-auto" src={chien} alt="photo daltonien" />
